c70c2d0257689518865d02ab220977cdac0362ca
mashiro <pycopper@gmail.com>
2022-03-16 23:54:15 +0900

    commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,10 +14,8 @@ from pathlib import Path
 import os
 import dj_database_url
 import django_heroku
-
 # Activate Django-Heroku.
-django_heroku.settings(locals())
-
+#django_heroku.settings(locals())
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_DIR = os.path.join(BASE_DIR,'static')
